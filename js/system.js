1804d3eeffeb90ddd650624e26cda7ef14bf24ab
suhyun_si <134688148+suhyun7512@users.noreply.github.com>
2024-03-27 10:23:39 +0900

    Update system.js

--- js/system.js
+++ js/system.js
@@ -1,21 +1,21 @@
-// const firebaseConfig = {
-//     apiKey: "AIzaSyBn6zoF7c7oj9NIhQ9BXHpn5jP6p_GDa6o",
-//     authDomain: "html-project-50a56.firebaseapp.com",
-//     databaseURL: "https://html-project-50a56-default-rtdb.firebaseio.com",
-//     projectId: "html-project-50a56",
-//     storageBucket: "html-project-50a56.appspot.com",
-//     messagingSenderId: "327952448390",
-//     appId: "1:327952448390:web:4dc6da31402ec9c4d50a07",
-//     measurementId: "G-RRNB3WY2S9"
-// };
-
 const firebaseConfig = {
-    apiKey: "AIzaSyA_Co_CZAlcv33j3ByLSoHif_e8NBFypGo",
-    authDomain: "html1-project.firebaseapp.com",
-    databaseURL: "https://html1-project-default-rtdb.firebaseio.com",
-    projectId: "html1-project",
-    storageBucket: "html1-project.appspot.com",
-    messagingSenderId: "905909966724",
-    appId: "1:905909966724:web:eb02b9499f80f90557efd1",
-    measurementId: "G-NX0Z7VEZS1"
+    apiKey: "AIzaSyBn6zoF7c7oj9NIhQ9BXHpn5jP6p_GDa6o",
+    authDomain: "html-project-50a56.firebaseapp.com",
+    databaseURL: "https://html-project-50a56-default-rtdb.firebaseio.com",
+    projectId: "html-project-50a56",
+    storageBucket: "html-project-50a56.appspot.com",
+    messagingSenderId: "327952448390",
+    appId: "1:327952448390:web:4dc6da31402ec9c4d50a07",
+    measurementId: "G-RRNB3WY2S9"
 };
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyA_Co_CZAlcv33j3ByLSoHif_e8NBFypGo",
+//     authDomain: "html1-project.firebaseapp.com",
+//     databaseURL: "https://html1-project-default-rtdb.firebaseio.com",
+//     projectId: "html1-project",
+//     storageBucket: "html1-project.appspot.com",
+//     messagingSenderId: "905909966724",
+//     appId: "1:905909966724:web:eb02b9499f80f90557efd1",
+//     measurementId: "G-NX0Z7VEZS1"
+// };
